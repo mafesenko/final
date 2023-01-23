660e4585e2c2bf9ba7a0bf8d03c3b4474f48c2e1
yupanasenko <123364029+yupanasenko@users.noreply.github.com>
2023-01-23 12:33:47 +0300

    Update example-27_hex_str.cpp

--- example-27_hex_str.cpp
+++ example-27_hex_str.cpp
@@ -4,9 +4,10 @@
 #include <libakrypt-base.h>
 
 int main() {
+    /*Строка для преобразования в hex*/
     char str[] = "394B50";
     char a[64];
-
+    /*Преобразование в hex*/
     ak_hexstr_to_ptr(str, a, sizeof(a), ak_true);
 
     for (int i = 0; i < sizeof(a); ++i)
